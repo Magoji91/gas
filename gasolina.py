aad3a4670a1e7550b94d1d87f38e2a08928ae1fa
seu-usuario-git <smsanda2004@gmail.com>
2023-12-27 11:45:30 +0000

    Arquivos gerados: base de dados gasolina.py gasolina.png - novo gráfico

--- gasolina.py
+++ gasolina.py
@@ -1,1 +1,21 @@
- 
+# código de geração do gráfico 
+import pandas as pd
+import seaborn as sns
+
+dados_gasolina = pd.read_csv('gasolina.csv')
+
+with sns.axes_style('darkgrid'):
+  grafico_gasolina = sns.lineplot(
+      x = 'Data',
+      y = 'Venda',
+      df = gas
+  )
+  
+  grafico_gasolina.set(
+      title = 'Valor Diário da Gasolina no Município de São Paulo',
+      xlabel = 'Dia',
+      ylabel = 'Preço (R$)'
+  )
+  
+  figura = grafico_gasolina.get_figure()    
+  figura.savefig('gasolina.png', dpi=600)
